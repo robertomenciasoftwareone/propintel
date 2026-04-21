2bcd97e02da6e7e2054a79d40ac79680d056f773
Roberto Mencia <robertomencia@gmail.com>
2026-04-21 10:54:33 +0200

    feat: update apiUrl in production environment configuration

--- propintel/src/environments/environment.production.ts
+++ propintel/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://REEMPLAZAR-CON-URL-RAILWAY.railway.app/api',
+  apiUrl: 'https://propintel-production-39e9.up.railway.app/api',
   apiKey: 'UrbIA-Prod-Temp-Key-2026',
   geminiApiKey: 'AIzaSyBR5ImZM-rsShEN1LRp336DlujstoBFzCI',  // Configura en las variables de entorno del pipeline CI/CD
 };
